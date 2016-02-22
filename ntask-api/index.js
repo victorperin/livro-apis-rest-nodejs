307cc3004c2d2bcb67f85b8232b55e5e07ec4dee
Victor Perin <me@victorperin.com>
2016-02-21 23:23:01 -0300

    finalmente inicio

---
+++ ntask-api/index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+const app = express();
+const port = 3000;
+
+
+app.get('/', (req, res) => res.json({status: 'NTask API'}));
+
+app.listen(port, () => console.log(`NTask API - porta ${port}`));
